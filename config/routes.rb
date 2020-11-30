bc61d0266aeacd3041a9afe6da419f53b592e431
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-29 22:01:06 -0800

    Update application view and routes to include login with github

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "works#root"
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
+  # get "/login", to: "users#login_form", as: "login"
+  # post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
 
+  # Login with omniauth
+  get '/auth/github', as: 'github_login'
+  get '/auth/:provider/callback', to: 'users#create', as: 'auth_callback'
+
+  # delete '/logout', to: 'merchants#destroy', as: 'logout'
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
 
